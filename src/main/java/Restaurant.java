89f443dec26c25ad7bc6599b33fe6c6553a86b77
Dakshina15 <dakshina@9611@gmail.com>
2021-04-05 16:30:26 +0530

    Part 2: Solution

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,86 @@
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+    private List<Item> Item = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        int closeTime = getCurrentTime().compareTo(this.closingTime);
+        int openTime = getCurrentTime().compareTo(this.openingTime);
+        return( closeTime < 0) && (openTime > 0 )? true : false;
+    }
+
+    public LocalTime getCurrentTime(){
+        return  LocalTime.now();
+    }
+
+    public List<Item> getMenu() {
+        return menu;
+
+        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+    }
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+
+    }
+
+    public void addToItem(String name, int price) {
+        Item newItem = new Item(name, price);
+        Item.add(newItem);
+    }
+
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getTotalOrderPrice(List<Item> listOfItems){
+        int totalSum = 0;
+        for(Item i : listOfItems) {
+            totalSum = totalSum + i.getPrice();
+        }
+        System.out.println("Total order Price" + totalSum);
+        return totalSum;
+    }
+
+}
